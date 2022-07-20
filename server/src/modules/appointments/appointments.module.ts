970f33f567802e8890d056446c5d22ee8edd1472
VietAnh188 <anhkun123456@gmail.com>
2022-07-20 14:11:59 +0700

    new:create middleware/permission to check if you are owner this appointment, denied request

--- server/src/modules/appointments/appointments.module.ts
+++ server/src/modules/appointments/appointments.module.ts
@@ -1,12 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { PersonsModule } from '../persons/persons.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { IsOwnerMiddleware } from '../../common/permissions/isOwner.middleware';
 
 @Module({
   controllers: [AppointmentsController],
   providers: [AppointmentsService],
   imports: [PersonsModule, PrismaModule],
 })
-export class AppointmentsModule {}
+export class AppointmentsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(IsOwnerMiddleware).forRoutes({
+      path: 'appointments/subscribe',
+      method: RequestMethod.POST,
+    });
+  }
+}
