7cbece4ae46c954c55488fafca37f36b70123e72
VietAnh188 <anhkun123456@gmail.com>
2022-07-24 09:12:56 +0700

    new:initial roles guard

--- server/src/common/guards/roles.guard.ts
+++ server/src/common/guards/roles.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
+// import { Reflector } from "@nestjs/core";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor (private reflector: Reflector) { }
+    // constructor (private reflector: Reflector) { }
 
     canActivate(context: ExecutionContext): boolean {
         return true;
