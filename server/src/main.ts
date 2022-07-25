7bad051f8f13c0a3b556a83b7b0314ea763e5cba
VietAnh188 <anhkun123456@gmail.com>
2022-07-25 19:05:31 +0700

    new:created middleware to log api request

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/apps/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(1808);
+    const app = await NestFactory.create(AppModule, {
+        logger: ['log', 'error', 'debug', 'verbose', 'warn']
+    });
+    await app.listen(1808);
 }
 bootstrap();
--- server/src/modules/apps/app.module.ts
+++ server/src/modules/apps/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -11,27 +11,32 @@ import { PersonsModule } from '../persons/persons.module';
 import { AuthsModule } from '../auths/auths.module';
 import { AppointmentsModule } from '../appointments/appointments.module';
 import { CachesModule } from '../caches/caches.module';
+import { ApiLogMiddleware } from '../../common/middlewares/apiLog.middleware';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      serveRoot: '/public/banners',
-      rootPath: path.join(__dirname, '..', '..', '..', 'public', 'banners'),
-    }),
-    ServeStaticModule.forRoot({
-      serveRoot: '/public/locations',
-      rootPath: path.join(__dirname, '..', '..', '..', 'public', 'locations'),
-    }),
-    PrismaModule,
-    ImagesModule,
-    LocationsModule,
-    AccountsModule,
-    PersonsModule,
-    AuthsModule,
-    AppointmentsModule,
-    CachesModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ServeStaticModule.forRoot({
+            serveRoot: '/public/banners',
+            rootPath: path.join(__dirname, '..', '..', '..', 'public', 'banners'),
+        }),
+        ServeStaticModule.forRoot({
+            serveRoot: '/public/locations',
+            rootPath: path.join(__dirname, '..', '..', '..', 'public', 'locations'),
+        }),
+        PrismaModule,
+        ImagesModule,
+        LocationsModule,
+        AccountsModule,
+        PersonsModule,
+        AuthsModule,
+        AppointmentsModule,
+        CachesModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(ApiLogMiddleware).forRoutes('*');
+    }
+}
