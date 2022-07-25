2071bb4edff374aa0621a07e730314bc75bb3880
VietAnh188 <anhkun123456@gmail.com>
2022-07-25 19:59:34 +0700

    update:add extend for flex

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,30 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      width: {
-        350: "350px",
-      },
-      height: {
-        line: "1px",
-      },
-      fontSize: {
-        tworem: "2rem",
-      },
-      borderWidth: {
-        1: "1px",
-      },
-      backgroundColor: {
-        primary: "#003580",
-      },
-      borderColor: {
-        primary: "#003580",
-      },
-      colors: {
-        primary: "#003580",
-      },
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            width: {
+                350: "350px",
+            },
+            height: {
+                line: "1px",
+            },
+            fontSize: {
+                tworem: "2rem",
+            },
+            borderWidth: {
+                1: "1px",
+            },
+            backgroundColor: {
+                primary: "#003580",
+            },
+            borderColor: {
+                primary: "#003580",
+            },
+            colors: {
+                primary: "#003580",
+            },
+            flex: {
+                '2': '2 2 0%'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
